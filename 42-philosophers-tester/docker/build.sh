d941f6e52eae31aaf676b0db497a15a1c71d6fb1
marcorondong <95254384+marcorondong@users.noreply.github.com>
2024-03-10 19:48:12 +0100

    Delete 42-philosophers-tester directory

--- 42-philosophers-tester/docker/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t memory-test:0.1 .
--- 42-philosophers-tester/visualizer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if command -v python3 >/dev/null 2>&1; then
-    pip3 install -r requirements.txt >/dev/null 2>&1
-    python3 visualizer.py
-else
-    echo "Python is not installed. Please install python to run the script."
-fi
